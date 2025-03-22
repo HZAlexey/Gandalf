5cc6b6c9860ad301783ca7fa4364bb4503da1003
Alexey <qa.alexey@yandex.ru>
2025-03-22 15:06:58 +0300

    Появилось модальное окно, в котором будет появляться OriginValue

---
+++ static/js/domElements.js
@@ -0,0 +1,4 @@
+export const changeTypeSelect = document.getElementById("change_type");
+export const linkToCardInput = document.getElementById("link_to_card");
+export const changeBranch = document.getElementById("change_branch");
+export const addRubricButton = document.getElementById("add-rubric");
